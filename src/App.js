82c50b934db9afd54c078b1b8cc61aa6931c61d2
Devin Halladay <devin@devinhalladay.com>
2019-01-26 22:18:47 -0500

    Add styled-components. Add state.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import Menu from './components/Menu'
+import Controls from './components/Controls'
+
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      showMenu: false
+    };
+  }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Menu show={this.state.showMenu}/>
+        <Controls />
       </div>
     );
   }
---
+++ src/components/Menu.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+
+import styled from "styled-components";
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+
+class Menu extends Component {
+  render() {
+    return <div className={this.props.show ? `menu open` : `menu`}>
+        <div className="breadcrumbs">
+          <nav>
+            <ul>
+              <li>
+                <a href="#">Home</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className="contents grid">
+          <div className="nav">
+            <div className="collapsible">
+              <header>
+                <h2>Investigate</h2>
+                <svg width="27" height="29" viewBox="0 0 27 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M13.5 28.6985V1.19849M13.5 1.19849L1 13.6985M13.5 1.19849L26 13.6985" stroke="black" />
+                </svg>
+              </header>
+              <div className="collapsible-content">
+                <ul>
+                  <li>
+                    <a href="#">Test</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="collapsible">
+              <header>
+                <h2>Participate</h2>
+                <svg width="27" height="29" viewBox="0 0 27 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M13.5 28.6985V1.19849M13.5 1.19849L1 13.6985M13.5 1.19849L26 13.6985" stroke="black" />
+                </svg>
+              </header>
+              <div className="collapsible-content">
+                <ul>
+                  <li>
+                    <a href="#">Test</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <h3>
+              <a href="#">About</a>
+            </h3>
+            <h3>
+              <a href="#">Partners</a>
+            </h3>
+          </div>
+          <div className="gc-s1" />
+          <div className="trending">
+            <h4 className="trending-header">
+              <svg width="21" height="14" viewBox="0 0 21 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M1 13.1985L9 5.19849L12.5 8.69849L20.5 0.698486M20.5 0.698486H15.5M20.5 0.698486V5.19849" stroke="black" />
+              </svg>
+              Trending
+            </h4>
+            <a href="/library/2012/01/01/example-article/" className="article-unit">
+              <div className="article-cover-image" style={{backgroundImage: 'url(/assets/images/test-1.jpg)'}} />
+              <p className="article-title">Example Article</p>
+            </a>
+            <a href="/library/2012/01/01/example-article/" className="article-unit">
+              {" "}
+            <div className="article-cover-image" style={{ backgroundImage: 'url(/assets/images/test-1.jpg)' }} />
+              <p className="article-title">Example Article</p>
+            </a>
+          </div>
+        </div>
+      </div>;
+  }
+}
+
+export default Menu;
